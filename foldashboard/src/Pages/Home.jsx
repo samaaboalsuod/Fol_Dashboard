a7ec117073bd23835282fbf6597bdb5ea97896e5
samaaboalsuod <samaaaboalsuod@gmail.com>
2026-04-05 15:44:53 +0200

    structure done

--- foldashboard/src/Pages/Home.jsx
+++ foldashboard/src/Pages/Home.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+// import { supabase } from "../Supabase";
+
 import './Home.css'
 
 const Home = () => {
     return (<>
     
     
-    
-    
-    
-    
-    
-    
+<section class="dashboard-wrapper ">
+
+  <aside> </aside>
+  
+  <main class="main-content">
+
+    <header></header>
+
+    <section class="dashboard-body"></section>
+
+  </main>
+
+</section>
+
     
     </>  );
 }
